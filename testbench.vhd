e7d54554ac4a1baaac684b963deb9c5d9491684e
RenanPicoli <renanps123@gmail.com>
2021-02-18 20:14:54 -0300

    testbench: commented out code of slave (not implemented yet); wired signals WS, SCK and SD; removed signals specific of I2C

--- testbench.vhd
+++ testbench.vhd
@@ -5,7 +5,6 @@ use ieee.numeric_std.all;
 use work.all;
 use work.my_types.all;
 
-
 entity testbench is
 end testbench;
 
@@ -16,21 +15,21 @@ constant TIME_DELTA : time := 5 us;
 --reset duration must be long enough to be perceived by the slowest clock (filter clock, both polarities)
 constant TIME_RST : time := 5 us;
 
-component i2c_slave
-	port (
-			D: in std_logic_vector(31 downto 0);--for register write
-			ADDR: in std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
-			CLK: in std_logic;--for register read/write, also used to generate SCL
-			RST: in std_logic;--reset
-			WREN: in std_logic;--enables register write
-			RDEN: in std_logic;--enables register read
-			IACK: in std_logic;--interrupt acknowledgement
-			Q: out std_logic_vector(31 downto 0);--for register read
-			IRQ: out std_logic;--interrupt request
-			SDA: inout std_logic;--open drain data line
-			SCL: inout std_logic --open drain clock line
-	);
-end component;
+--component i2s_slave_receiver --TO-DO
+--	port (
+--			D: in std_logic_vector(31 downto 0);--for register write
+--			ADDR: in std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
+--			CLK: in std_logic;--for register read/write, also used to generate SCL
+--			RST: in std_logic;--reset
+--			WREN: in std_logic;--enables register write
+--			RDEN: in std_logic;--enables register read
+--			IACK: in std_logic;--interrupt acknowledgement
+--			Q: out std_logic_vector(31 downto 0);--for register read
+--			IRQ: out std_logic;--interrupt request
+--			SDA: inout std_logic;--open drain data line
+--			SCL: inout std_logic --open drain clock line
+--	);
+--end component;
 
 --master signals
 signal D: std_logic_vector(31 downto 0);--for register write
@@ -42,23 +41,18 @@ signal RDEN: std_logic;--enables register read
 signal IACK: std_logic;--interrupt acknowledgement
 signal Q: std_logic_vector(31 downto 0);--for register read
 signal IRQ: std_logic;--interrupt request
-signal SDA: std_logic;--open drain data line
-signal SCL: std_logic;--open drain clock line
+signal SD: std_logic;--data line
+signal WS: std_logic;--word select (left/right clock)
+signal SCK: std_logic;--bit clock
 
 --slave signals
-signal D_slv: std_logic_vector(31 downto 0);--for register write
-signal ADDR_slv: std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
-signal WREN_slv: std_logic;--enables register write
+--signal D_slv: std_logic_vector(31 downto 0);--for register write
+--signal ADDR_slv: std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
+--signal WREN_slv: std_logic;--enables register write
 
-signal read_mode: std_logic;
-signal write_mode: std_logic;
-constant RW_bit: std_logic:='0';-- 1 read mode; 0 write mode
 begin
-	--all these times are relative to the beginning of simulation
-	--'H' models the pull up resistor in SDA line
-	SDA <= 'H';--pull up resistor
 	
-	DUT: entity work.i2c_master
+	DUT: entity work.i2s_master_transmitter
 	port map(D 		=> D,
 				CLK	=> CLK,
 				ADDR 	=> ADDR,
@@ -68,23 +62,24 @@ begin
 				IACK	=> IACK,
 				Q		=>	Q,
 				IRQ	=>	IRQ,
-				SDA	=>	SDA,
-				SCL	=>	SCL
+				SD		=>	SD,
+				WS		=> WS,
+				SCK	=>	SCK
 	);
 	
-	slave: i2c_slave
-	port map(D 		=> D_slv,
-				CLK	=> CLK,
-				ADDR 	=> ADDR_slv,
-				RST	=>	RST,
-				WREN	=> WREN_slv,
-				RDEN	=>	RDEN,
-				IACK	=> IACK,
-				Q		=>	open,
-				IRQ	=>	open,
-				SDA	=>	SDA,
-				SCL	=>	SCL
-	);
+--	slave: i2c_slave
+--	port map(D 		=> D_slv,
+--				CLK	=> CLK,
+--				ADDR 	=> ADDR_slv,
+--				RST	=>	RST,
+--				WREN	=> WREN_slv,
+--				RDEN	=>	RDEN,
+--				IACK	=> IACK,
+--				Q		=>	open,
+--				IRQ	=>	open,
+--				SDA	=>	SDA,
+--				SCL	=>	SCL
+--	);
 	
 	clock: process--200kHz input clock (common to slave and master)
 	begin
@@ -94,10 +89,7 @@ begin
 		wait for 2.5 us;
 	end process clock;
 	
-	--I2C registers configuration
-	read_mode <= RW_bit;
-	write_mode <= not read_mode;
-	
+	--I2S registers configuration	
 	master_setup:process
 	begin
 		--zeroes & WORDS & SLV ADDR & R/W(1 read mode; 0 write mode)
@@ -106,7 +98,7 @@ begin
 		WREN <= '1';
 		wait for TIME_RST + TIME_DELTA;
 		
-		--bits 7:0 data to be transmitted
+		--bits 7:0 data to be transmitted (goes to fifo)
 		ADDR <= "01";--DR address	
 		D <= x"0000_0095";-- 1001 0101	
 		WREN <= '1';
@@ -121,26 +113,27 @@ begin
 		wait;--process executes once
 	end process master_setup;
 	
-	slave_setup:process
-	begin
-		--zeroes & WORDS & OADDR & R/W(must store RW bit sent by master; 1 read mode; 0 write mode)
-		ADDR_slv <= "00";--CR address
-		D_slv <= (31 downto 10 =>'0') & "01" & "0000101" & 'X';--WORDS: 01; OADDR: 0101
-		WREN_slv <= '1';
-		wait for TIME_RST + TIME_DELTA;
-		
-		ADDR_slv <= "01";--DR address
-		--bits 7:0 data received or to be read by master	
-		D_slv <= x"0000_00A4";-- data to be read by master
-		WREN_slv <= '1';
-		wait for TIME_DELTA;
-
-		ADDR_slv<="11";--invalid address
-		D_slv<=(others=>'0');
-		WREN_slv <= '0';
-		wait for TIME_DELTA;
-		wait;--process executes once
-	end process slave_setup;
+--	slave_setup:process
+--	begin
+--		--zeroes & WORDS & OADDR & R/W(must store RW bit sent by master; 1 read mode; 0 write mode)
+--		ADDR_slv <= "00";--CR address
+--		D_slv <= (31 downto 10 =>'0') & "01" & "0000101" & 'X';--WORDS: 01; OADDR: 0101
+--		WREN_slv <= '1';
+--		wait for TIME_RST + TIME_DELTA;
+--		
+--		ADDR_slv <= "01";--DR address
+--		--bits 7:0 data received or to be read by master	
+--		D_slv <= x"0000_00A4";-- data to be read by master
+--		WREN_slv <= '1';
+--		wait for TIME_DELTA;
+--
+--		ADDR_slv<="11";--invalid address
+--		D_slv<=(others=>'0');
+--		WREN_slv <= '0';
+--		wait for TIME_DELTA;
+--		wait;--process executes once
+--	end process slave_setup;
+	
 	RST <= '1', '0' after TIME_RST;--reset common to slave and master
 	
 end architecture test;
